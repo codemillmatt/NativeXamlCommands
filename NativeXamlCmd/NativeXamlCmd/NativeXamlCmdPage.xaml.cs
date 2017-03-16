62c34e37f0504e98dce2189bc37fe6e9dfdaa8fc
Matt Soucoup <msoucoup@codemilltech.com>
2017-03-16 05:28:12 -0500

    xamlc

--- NativeXamlCmd/NativeXamlCmd/NativeXamlCmdPage.xaml.cs
+++ NativeXamlCmd/NativeXamlCmd/NativeXamlCmdPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace NativeXamlCmd
 {
+	[XamlCompilation(XamlCompilationOptions.Skip)]
 	public partial class NativeXamlCmdPage : ContentPage
 	{
 		public NativeXamlCmdPage()
--- NativeXamlCmd/NativeXamlCmd/Properties/AssemblyInfo.cs
+++ NativeXamlCmd/NativeXamlCmd/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
+using Xamarin.Forms.Xaml;
 
 [assembly: AssemblyTitle("NativeXamlCmd")]
 [assembly: AssemblyDescription("")]
@@ -24,3 +25,5 @@ using System.Runtime.CompilerServices;
 
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
